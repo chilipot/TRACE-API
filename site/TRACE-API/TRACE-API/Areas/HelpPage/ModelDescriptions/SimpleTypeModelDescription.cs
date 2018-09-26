7227df624c12e3c4b5cf00568e534664650eaed5
Unix-Code <dmalakh@comcast.net>
2018-09-26 16:20:25 -0400

    New .NET site app

---
+++ site/TRACE-API/TRACE-API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TRACE_API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ site/TRACE-API/TRACE-API/Models/Term_Annotations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TRACE_API.Models
+{
+    [MetadataType(typeof(TermMetadata))]
+    public partial class Term
+    {
+
+    }
+
+    public class TermMetadata
+    {
+
+    }
+}
