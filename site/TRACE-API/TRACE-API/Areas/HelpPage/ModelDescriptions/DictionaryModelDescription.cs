a195f6e62ec87bfe652c2578a406e897f1b5b17d
Unix-Code <dmalakh@comcast.net>
2019-02-22 22:35:55 -0500

    Cleaned up repo/master a bunch

--- site/TRACE-API/TRACE-API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace TRACE_API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- site/TRACE-API/TRACE-API/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TRACE_API.WebApiApplication" Language="C#" %>
