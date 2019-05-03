9d38b911377e4a2086d1ea503ff6ac5d68d94d67
Unix-Code <dmalakh@comcast.net>
2019-05-03 19:19:14 -0400

    Refactored searching/highlighting to be more DRY with a base class
    Signed-off-by: Unix-Code <dmalakh@comcast.net>

---
+++ trace/api/model/mixins/BaseSearchable.py
@@ -0,0 +1,55 @@
+from elasticsearch_dsl import Search
+from flask import current_app
+
+
+class BaseSearchable(object):
+    # These should all be defined in a subclass
+    INDEX = ''
+    ID_FIELD_NAME = ''
+    INDEXED_FIELDS = []
+
+    @classmethod
+    def _perform_search(cls, expr, page, per_page, highlights=False):
+        return cls.elastic_search(expr, cls.INDEXED_FIELDS, page, per_page, cls.ID_FIELD_NAME, cls.INDEX,
+                                  highlights=highlights)
+
+    @classmethod
+    def highlights(cls, expr, page, per_page):
+        search_result = cls._perform_search(expr, page, per_page, highlights=True)
+        # Only select highlights for top 10 search results
+        highlights_results = [x.meta for x in search_result[:10] if x.meta.highlight]
+        highlights = {field: {} for field in cls.INDEXED_FIELDS}
+        for h in highlights_results:
+            for key, val in h.highlight.to_dict().items():
+                if highlights[key].get(val[0], -1) < h.score:
+                    highlights[key][val[0]] = h.score
+        for key, val in highlights.items():
+            highlights[key] = [pair[0] for pair in sorted(val.items(), key=lambda x: x[1], reverse=True)]
+        return highlights
+
+    @classmethod
+    def search(cls, expr, page, per_page):
+        search_result = cls._perform_search(expr, page, per_page, highlights=False)
+        ids = [x[cls.ID_FIELD_NAME] for x in search_result]
+        return cls.map_all_objects(ids) if ids else []
+
+    @classmethod
+    def map_all_objects(cls, ids):
+        query_result = cls.query.filter(cls.id.in_(ids)).all()
+        object_map = {o.id: o for o in query_result}
+        sql_objects = [object_map[obj_id] for obj_id in ids]
+        return sql_objects
+
+    @classmethod
+    def construct_query(cls, query, _fields_list):
+        raise NotImplementedError('Please implement a way to construct an ElasticSearch query for this class')
+
+    @classmethod
+    def elastic_search(cls, query, fields_list, page, per_page, id_field_name, index, highlights=False):
+        full_query = cls.construct_query(query, fields_list)
+        search_query = Search(using=current_app.elasticsearch, index=index).query(full_query).source(id_field_name)
+        if highlights:
+            for field in fields_list:
+                search_query = search_query.highlight(field)
+
+        return search_query[((page - 1) * per_page):(page * per_page)].execute()
--- trace/api/model/mixins/DepartmentSearchable.py
+++ trace/api/model/mixins/DepartmentSearchable.py
@@ -1,54 +1,13 @@
-from elasticsearch_dsl import Search
 from elasticsearch_dsl.query import MultiMatch
-from flask import current_app
 
-INDEXED_FIELDS = ['title', 'code']
+from api.model.mixins.BaseSearchable import BaseSearchable
 
 
-class DepartmentSearchable(object):
-    @classmethod
-    def _perform_search(cls, expr, page, per_page, highlights=False):
-        return cls.elastic_search(expr, INDEXED_FIELDS, page, per_page, highlights=highlights)
-
-    @classmethod
-    def highlights(cls, expr, page, per_page):
-        search_result = cls._perform_search(expr, page, per_page, highlights=True)
-        # Only select highlights for top 10 search results
-        highlights_results = [x.meta for x in search_result[:10] if x.meta.highlight]
-        highlights = {field: {} for field in INDEXED_FIELDS}
-        for h in highlights_results:
-            for key, val in h.highlight.to_dict().items():
-                if highlights[key].get(val[0], -1) < h.score:
-                    highlights[key][val[0]] = h.score
-        for key, val in highlights.items():
-            highlights[key] = [pair[0] for pair in sorted(val.items(), key=lambda x: x[1], reverse=True)]
-        return highlights
-
-    @classmethod
-    def search(cls, expr, page, per_page):
-        search_result = cls._perform_search(expr, page, per_page, highlights=False)
-        ids = [x['department_id'] for x in search_result]
-        return cls.map_all_departments(ids) if ids else []
-
-    @classmethod
-    def map_all_departments(cls, ids):
-        query_result = cls.query.filter(cls.id.in_(ids)).all()
-        object_map = {o.id: o for o in query_result}
-        sql_objects = [object_map[obj_id] for obj_id in ids]
-        return sql_objects
+class DepartmentSearchable(BaseSearchable):
+    INDEXED_FIELDS = ['title', 'code']
+    ID_FIELD_NAME = 'department_id'
+    INDEX = 'department'
 
     @classmethod
     def construct_query(cls, query, _fields_list):
-        match_query = MultiMatch(query=query, fields=['title', 'code'])
-        return match_query
-
-    @classmethod
-    def elastic_search(cls, query, fields_list, page, per_page, id_field_name='department_id', highlights=False,
-                       index='department'):
-        full_query = cls.construct_query(query, fields_list)
-        search_query = Search(using=current_app.elasticsearch, index=index).query(full_query).source(id_field_name)
-        if highlights:
-            for field in fields_list:
-                search_query = search_query.highlight(field)
-
-        return search_query[((page - 1) * per_page):(page * per_page)].execute()
+        return MultiMatch(query=query, fields=['title', 'code'])
