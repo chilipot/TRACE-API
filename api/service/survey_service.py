e40d83be67ad2ec4665729a03d44f42ad3f30490
Unix-Code <dmalakh@comcast.net>
2019-05-17 22:59:05 -0400

    added categories endpoint

--- api/service/survey_service.py
+++ api/service/survey_service.py
@@ -1,4 +1,4 @@
-from api.model import LookupQuestion
+from api.model import LookupQuestion, QuestionCategory
 from api.utils.helpers import sort_and_paginate, apply_sql_facets
 
 
@@ -7,3 +7,7 @@ def get_all_questions(page, page_size, order_by, facets={}):
     if facets:
         query = apply_sql_facets(LookupQuestion, query, facets)
     return [q.as_dict() for q in sort_and_paginate(query, order_by, page, page_size).all()]
+
+
+def get_all_categories(page, page_size, order_by):
+    return [c.as_dict() for c in sort_and_paginate(QuestionCategory.query, order_by, page, page_size).all()]
